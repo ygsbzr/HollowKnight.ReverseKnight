6d44df2e0668cc3a963dc9b3ec9d87cf3202026e
YG <1440412630@qq.com>
2022-06-21 21:18:20 +0800

    Initial

---
+++ ReverseKnight/KnightListener.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+namespace ReverseKnight
+{
+    public class KnightListener:MonoBehaviour
+    {
+        void Update()
+        {
+            if(!HeroController.instance.cState.touchingWall&& HeroController.instance.cState.facingRight)
+            {
+                Vector3 scale = HeroController.instance.transform.localScale;
+                scale.x = 1f;
+                HeroController.instance.transform.localScale = scale;
+            }
+            else if(!HeroController.instance.cState.touchingWall && !HeroController.instance.cState.facingRight)
+            {
+                Vector3 scale = HeroController.instance.transform.localScale;
+                scale.x = -1f;
+                HeroController.instance.transform.localScale = scale;
+            }
+        }
+    }
+}
---
+++ ReverseKnight/ReverseKnight.cs
@@ -0,0 +1,26 @@
+﻿using Modding;
+using System.Reflection;
+using UnityEngine;
+namespace ReverseKnight
+{
+    public class ReverseKnight:Mod
+    {
+        public override string GetVersion()
+        {
+            return "1.0";
+        }
+        public override void Initialize()
+        {
+            On.HeroController.Start += AddListener;
+        }
+
+        private void AddListener(On.HeroController.orig_Start orig, HeroController self)
+        {
+            orig(self);
+            if(self.gameObject.GetComponent<KnightListener>()==null)
+            {
+                self.gameObject.AddComponent<KnightListener>();
+            }
+        }
+    }
+}
